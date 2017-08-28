a94e1f58d86932d0cc23d18afd479391f7a97a91
Marcos Sñánchez <msanchez@katodia.com>
2017-08-28 18:47:25 +0200

    Swap library

--- bot.js
+++ bot.js
@@ -1,11 +1,5 @@
-var Twitter = require('twitter');
-
-var config = {
-    consumer_key: process.env.CONSUMER_KEY,
-    consumer_secret: process.env.CONSUMER_SECRET,
-    access_token_key: process.env.ACCESS_TOKEN,
-    access_token_secret: process.env.ACCESS_SECRET
-};
+var config = require('./config.js');
+var Twitter = require('twit');
 
 var client = new Twitter(config);
 
---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  consumer_key: 'Qczoxm3mCtN5pz4LDPR4lNezZ',  
+  consumer_secret: '3NYprKgbwdHlPrl3ePCetscUuU80zQDXu2tf5gyaQOgQwnGDRQ',
+  access_token: '634865853-GyHbKYvynXIGhj3LijbfIx5OlFJnPBLHBw8Fd4Z0',  
+  access_token_secret: 'XOt0RfqPv5GXC0RRK2XHyLBXIlro7s2KAN3g5S3Rnb6Jo'
+};
+
